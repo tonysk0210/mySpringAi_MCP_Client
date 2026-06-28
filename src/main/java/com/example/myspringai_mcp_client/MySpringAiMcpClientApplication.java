69d9b07f92133a1f2ad3dafe990ddd0e9bd62eeb
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-06-28 13:49:08 +0800

    git init

---
+++ src/main/java/com/example/myspringai_mcp_client/MySpringAiMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringai_mcp_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringAiMcpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringAiMcpClientApplication.class, args);
+    }
+
+}
